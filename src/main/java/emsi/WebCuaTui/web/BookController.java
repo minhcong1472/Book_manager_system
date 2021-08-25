2adb1902a4fd81f7d7930747a9f75d8fb0b574ac
minhcong1472 <jinjnguyen147@gmail.com>
2021-08-25 10:47:08 +0700

    full chuc nang

--- src/main/java/emsi/WebCuaTui/web/BookController.java
+++ src/main/java/emsi/WebCuaTui/web/BookController.java
@@ -1,10 +1,11 @@
 package emsi.WebCuaTui.web;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,12 +16,25 @@ import org.springframework.web.servlet.ModelAndView;
 
 import emsi.WebCuaTui.Service.BookService;
 import emsi.WebCuaTui.model.Book;
+import emsi.WebCuaTui.model.BookCase;
+import emsi.WebCuaTui.model.User;
+import emsi.WebCuaTui.repository.BookCaseRepository;
+import emsi.WebCuaTui.repository.BookRepositoty;
+import emsi.WebCuaTui.repository.UserRepository;
 
 @Controller
 
 public class BookController {
+	
+	@Autowired
+	BookCaseRepository bcRepo;
 
-	Book book = new Book();
+	@Autowired
+	UserRepository userRepo;
+
+	@Autowired
+	BookRepositoty bookRepo;
+	
 	@Autowired
 	BookService service;
 	
@@ -78,9 +92,20 @@ public class BookController {
 	@RequestMapping("/delete/{book_id}")
 	public String deleteBook(@PathVariable(name="book_id")Long book_id , Model model , @Param("keyword") String keyword) {
 		service.delete(book_id);
+		
+		Book book = bookRepo.findById(book_id).orElse(null);
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		org.springframework.security.core.userdetails.User au = (org.springframework.security.core.userdetails.User) authentication
+				.getPrincipal();
+		User user = userRepo.findByEmail(au.getUsername());
+		BookCase bookcase = new BookCase();
+		bookcase = bcRepo.findByUser(user);
+		bookcase.getBooks().remove(book);
+		bcRepo.save(bookcase);
+		
 		List<Book> listbook = service.listAll(keyword);
 		model.addAttribute("listbook",listbook);
-		return"book";
+		return"redirect:/book";
 	}
 	
 }
